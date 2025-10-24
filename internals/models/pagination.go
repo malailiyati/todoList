76da2a888ed80e6013e9a0015588f739e2766b6b
malailiyati <malailiyati107@gmail.com>
2025-10-24 19:31:09 +0700

    feat(models): add Pagination struct for paginated responses

---
+++ internals/models/pagination.go
@@ -0,0 +1,8 @@
+package models
+
+type Pagination struct {
+	CurrentPage int `json:"current_page"`
+	PerPage     int `json:"per_page"`
+	Total       int `json:"total"`
+	TotalPages  int `json:"total_pages"`
+}
